3d5cc05d0c071a85daeda47c47c2e86fc9546e99
poode <eng.abdul.azeem@gmail.com>
2020-06-12 00:07:02 +0200

    fixing bug

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ require('dotenv').config();
 const { PORT } = require('./config/constant');
 const { app } = require('./app');
 
-app.listen(PORT, () => console.log(`server is up on ${process.env.SSL ? 'https' : 'http'}://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`server is up on ${JSON.parse(process.env.SSL) ? 'https' : 'http'}://localhost:${PORT}`));
