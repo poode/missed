f5ba8ca7944b86068df81f89abdc2d3a789baf93
poode <eng.abdul.azeem@gmail.com>
2020-06-22 21:45:22 +0200

    configured ssl

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ require('dotenv').config();
 const { PORT } = require('./config/constant');
 const { app } = require('./app');
 
-
-const credentials = {
-  key: fs.readFileSync(path.resolve(`${__dirname}/config/ssl/ropsystem.com.key`), 'utf8'),
-  cert: fs.readFileSync(path.resolve(`${__dirname}/config/ssl/ropsystem.com.crt`), 'utf8'),
-};
-
-const secureServer = https.createServer(credentials, app);
-
-secureServer.listen(PORT, () => console.log(`server is up on ${JSON.parse(process.env.SSL) ? 'https' : 'http'}://localhost:${PORT}`));
+if(JSON.parse(process.env.SSL)) {
+  const credentials = {
+    key: fs.readFileSync(path.resolve(`${__dirname}/config/ssl/ropsystem.com.key`), 'utf8'),
+    cert: fs.readFileSync(path.resolve(`${__dirname}/config/ssl/ropsystem.com.crt`), 'utf8'),
+  };
+  
+  const secureServer = https.createServer(credentials, app);
+  
+  secureServer.listen(PORT, () => console.log(`server is up on https://localhost:${PORT}`));  
+} else {
+  app.listen(PORT, () => console.log(`server is up on http://localhost:${PORT}`));  
+}
